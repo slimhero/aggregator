a288b94f5864c6653d30098105490170ca78b761
SlimHero <nu_ne_figa_sebe@mail.ru>
2014-10-12 22:22:19 +0600

    scheduler works at test

--- scheduler.rb
+++ scheduler.rb
@@ -2,8 +2,8 @@
 
 require 'rufus-scheduler'
 require 'sqlite3'
+require 'logger'
 require_relative 'db/query'
-#require_relative 'parser/query'
 
 
 
@@ -14,36 +14,62 @@ module Schedule
 		
     def initialize
 			@scheduler = Rufus::Scheduler.new
+			@logger = Logger.new( './log.log' );
+			@logger.info( 'initialize' )
 		end
    
 		def run
 			# Load file
 			# load File.join( File.dirname(__FILE__), "./dbparser.rb" )
 			# Set interval for start
-			#@scheduler.interval '10s' do
-			@scheduler.in '5h' do
-				load File.join( File.dirname(__FILE__), "./dbparser.rb" )
-				# Queries
-				query = DB.Query
-				q = query[:POINTS_Q]
-				SQLite3::Database.new "./de.sqlite3" do |db|
-					db.execute( q ) do |data|
-					#db.query(q) do |data|
-puts ":0 #{data[0]}"
-puts ":1 #{data[1]}"
-puts ":2 #{data[2]}"
-puts ":3 #{data[3]}"
-puts ":4 #{data[4]}"
-puts ":5 #{data[5]}"
-puts ":6 #{data[6]}"
-puts "#{DB.Query}"
-puts "#{DB.Fields}"
-				  	txt = data[6]#pts["parser"]
-				  	name = data[2]#pts["label"]
-				  	pathdata = "./assets/data/#{data[3]}/"
-				  	forimg = "/data/#{data[3]}/"	
-						RestAPI::DBParser.new( data[0], name, txt, db, DB.Query, DB.Fields, pathdata, forimg, true )
+			@logger.info( 'run' )
+			@scheduler.interval '20s' do
+			#@scheduler.in '10s' do
+			#@scheduler.in '5h' do
+				begin
+					@logger.info "Dir.pwd = #{ Dir.pwd }" 
+					@logger.info "Dir.pwd = #{ Dir.pwd }" 
+					@logger.info "#{File.join( File.dirname(__FILE__), "./dbparser.rb" ) }"
+					load File.join( File.dirname(__FILE__), "./dbparser.rb" )
+					# Queries
+					query = DB.Query
+					q = query[:POINTS_Q]
+					SQLite3::Database.new "./de.sqlite3" do |db|
+						db.execute( q ) do |data|
+						#db.query(q) do |data|
+							@logger.info ":0 #{data[0]}"
+							@logger.info ":1 #{data[1]}"
+							@logger.info ":2 #{data[2]}"
+							@logger.info ":3 #{data[3]}"
+							@logger.info ":4 #{data[4]}"
+							@logger.info ":5 #{data[5]}"
+							@logger.info ":6 #{data[6]}"
+							@logger.info "#{DB.Query}"
+							@logger.info "#{DB.Fields}"
+							@logger.info "!!! 1"
+				  		txt = data[6]#pts["parser"]
+							@logger.info "!!! 2"
+				  		name = data[2]#pts["label"]
+							@logger.info "!!! 3"
+				  		pathdata = "./assets/data/#{data[3]}/"
+							@logger.info "!!! 4"
+				  		forimg = "/data/#{data[3]}/"
+						  @logger.info "start class"	
+							RestAPI::DBParser.new( 
+								data[0], 
+								name, 
+								txt, 
+								db, 
+								DB.Query, 
+								DB.Fields, 
+								pathdata, 
+								forimg, 
+								true 
+							)
+						end
 					end
+					rescue Exception => e
+						@logger.error( "#{e}" )
 				end
 			end
 
@@ -51,6 +77,10 @@ puts "#{DB.Fields}"
     end
 	end
 end
+
+#if Dir.pwd == "/"
+#	Dir.chdir "/home/develop/projects/agregator"
+#end
 
 s = Schedule::Scheduler.new
 s.run
